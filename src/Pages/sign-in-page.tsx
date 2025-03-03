38fbda0c4f112cecffe17bcf014e68a40d0b225a
vamsy yandamuri <vamsyyandamuri@vamsys-Laptop.local>
2025-03-03 00:15:31 -0800

    removed dark mode from login page

--- src/Pages/sign-in-page.tsx
+++ src/Pages/sign-in-page.tsx
@@ -80,9 +80,6 @@ export default function SignInPage() {
 
   return (
     <>
-    <div className="float-right m-3 mr-5">
-       <ModeToggle/>
-    </div>
     <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
       <div
         className={`w-full ${
